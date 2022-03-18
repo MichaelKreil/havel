bd1c639cf11d7e9a4b1739502319135e5ef68abe
Michael Kreil <github@michael-kreil.de>
2022-03-18 20:43:33 +0100

    Update test.js

--- bin/test.js
+++ bin/test.js
@@ -6,8 +6,18 @@ havel.pipeline(
 	havel.readFile('../data/friends_10000.tsv.xz', {showProgress:true}),
 	havel.decompressXZ(),
 	havel.split(),
-	//havel.head(10),
-	havel.map(line => havel.KeyValue(...line.split('\t')), {outputType:'keyValue'} ),
+	havel.map(line => {
+		line = line.split('\t');
+		let key = line[0];
+		let value = JSON.parse(line[1]);
+		return new havel.KeyValue(key, value)
+	}, {outputType:'keyValue'} ),
+	havel.eachPairWise((e1, e2) => {
+		if (e1.key.length < e2.key.length) return;
+		if ((e1.key.length > e2.key.length) || (e1.key >= e2.key)) {
+			throw Error(`${JSON.stringify(e1.key)} >= ${JSON.stringify(e2.key)}`);
+		}
+	}),
 	havel.keyValueToStream(),
 	havel.writeFile('../data/friends.bin'),
 	() => console.log('finished')
