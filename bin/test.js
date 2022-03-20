2a1cbb8432a2d3d397011eb5664dc794dff9652e
Michael Kreil <github@michael-kreil.de>
2022-03-20 21:27:59 +0100

    cleanup

--- bin/test.js
+++ bin/test.js
@@ -2,10 +2,8 @@
 
 const havel = require('../index.js');
 
-havel.pipeline(
-	havel.readFile('../data/friends_10000.tsv.xz', {
-		showProgress: true
-	}),
+havel.pipeline([
+	havel.readFile('../data/friends_10000.tsv.xz', { showProgress: true }),
 	havel.decompressXZ(),
 	havel.split(),
 	havel.map(line => {
@@ -13,22 +11,13 @@ havel.pipeline(
 		let key = line[0];
 		let value = JSON.parse(line[1]);
 		return new havel.KeyValue(key, value)
-	}, {
-		outputType: 'keyValue'
-	}),
+	}, {outputType:'keyValue'} ),
 	havel.eachPairWise((e1, e2) => {
 		if (e1.key.length < e2.key.length) return;
 		if ((e1.key.length > e2.key.length) || (e1.key >= e2.key)) {
 			throw Error(`${JSON.stringify(e1.key)} >= ${JSON.stringify(e2.key)}`);
 		}
 	}),
-	//havel.keyValueToBuffer(),
-	//havel.keyValueToStream(),
-	//havel.log(),
-	//havel.streamToKeyValue(),
-	//havel.eachPromise(e => e.getValuePromise()),
-	//havel.log(),
-	havel.dump(),
-	//havel.writeFile('../data/friends.bin'),
-	() => console.log('finished')
-)
+	havel.keyValueToStream(),
+	havel.writeFile('../data/friends_10000.bin'),
+], () => console.log('finished'))
