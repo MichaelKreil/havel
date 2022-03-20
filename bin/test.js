718d68f6fb623742b2d81bf46653d196c75e7e3e
Michael Kreil <github@michael-kreil.de>
2022-03-20 21:28:08 +0100

    rename

--- bin/test.js
+++ bin/test.js
@@ -1,23 +1,23 @@
 "use strict"
 
-const havel = require('../index.js');
+const Havel = require('../index.js');
 
-havel.pipeline([
-	havel.readFile('../data/friends_10000.tsv.xz', { showProgress: true }),
-	havel.decompressXZ(),
-	havel.split(),
-	havel.map(line => {
+Havel.pipeline([
+	Havel.readFile('../data/friends_10000.tsv.xz', { showProgress: true }),
+	Havel.decompressXZ(),
+	Havel.split(),
+	Havel.map(line => {
 		line = line.split('\t');
 		let key = line[0];
 		let value = JSON.parse(line[1]);
-		return new havel.KeyValue(key, value)
+		return new Havel.KeyValue(key, value)
 	}, {outputType:'keyValue'} ),
-	havel.eachPairWise((e1, e2) => {
+	Havel.eachPairWise((e1, e2) => {
 		if (e1.key.length < e2.key.length) return;
 		if ((e1.key.length > e2.key.length) || (e1.key >= e2.key)) {
 			throw Error(`${JSON.stringify(e1.key)} >= ${JSON.stringify(e2.key)}`);
 		}
 	}),
-	havel.keyValueToStream(),
-	havel.writeFile('../data/friends_10000.bin'),
+	Havel.keyValueToStream(),
+	Havel.writeFile('../data/friends_10000.bin'),
 ], () => console.log('finished'))
