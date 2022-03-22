fc25afa600165748332542f70213ed725cdecdcb
Michael Kreil <github@michael-kreil.de>
2022-03-22 21:29:21 +0100

    solve #1

--- test/test_keyValue.js
+++ test/test_keyValue.js
@@ -11,7 +11,7 @@ const getLetters = n => {
 	return String.fromCharCode(...chars)
 }
 const listIn = [];
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 128; i++) {
 	listIn[i] = new Havel.KeyValue(
 		getLetters(8),
 		Object.fromEntries(Array.from({length:4}, () =>
@@ -23,7 +23,7 @@ for (let i = 0; i < 256; i++) {
 describe('keyValue', () => {
 
 	helper.checkCompleteness('../lib/keyValue.js',
-		'bufferToKeyValue,keyValueToBuffer,keyValueToStream,streamToKeyValue,keyValueCheckOrder'
+		'bufferToKeyValue,keyValueToBuffer,keyValueToStream,streamToKeyValue,keyValueCheckOrder,keyValueMerge'
 	);
 
 	describe('keyValueToBuffer | bufferToKeyValue', () => {
@@ -45,13 +45,13 @@ describe('keyValue', () => {
 	})
 
 	describe('keyValueCheckOrder', () => {
-		it('check correct', done => {
+		it('should be valid', done => {
 			Havel.pipeline()
 				.fromArray([-1,0,1,2,3].map(id => new Havel.KeyValue(id,'nix')))
 				.keyValueCheckOrder((a,b) => a < b)
 				.dump().finished(done)
 		})
-		it('check incorrect', done => {
+		it('should be invalid', done => {
 			Havel.pipeline()
 				.fromArray([-1,0,0,2,3].map(id => new Havel.KeyValue(id,'nix')))
 				.keyValueCheckOrder((a,b) => a < b)
@@ -75,6 +75,23 @@ describe('keyValue', () => {
 					step(4);
 				})
 				.finished(() => step(5, done))
+		})
+	})
+
+	describe('keyValueMerge', () => {
+		it('should works', done => {
+			Havel.pipeline()
+				.fromArray([0,1,2,4,5].map(id => new Havel.KeyValue(id,'nix')))
+				.keyValueMerge(
+					Havel.pipeline().fromArray([0,1,3,4].map(id => new Havel.KeyValue(id,'nix'))),
+					(a,b) => a-b,
+					(e1,e2) => {
+						if (e1 && e2 && (e1.key !== e2.key)) assert.fail('if both entries, then their keys should be equal');
+						return e1 || e2;
+					}
+				)
+				.keyValueCheckOrder((a,b) => a < b)
+				.dump().finished(done)
 		})
 	})
 })
